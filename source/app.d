46682e102639cb90524ba8b0717dc4fed951674c
ottu <ottottu@gmail.com>
2015-03-09 16:31:28 +0900

    working.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,56 @@
 import std.stdio;
+import std.json;
+import std.algorithm;
+import std.string;
+import std.conv;
+import std.file;
+import std.range;
+import std.array;
 
 void main()
 {
-	writeln("Edit source/app.d to start your project.");
+    auto list = readText("./list.json");
+    auto json = parseJSON(list);
+
+    auto f = File("./news.csv");
+    JSONValue[] news = [];
+    foreach( line; f.byLine ) {
+        string[3] parsed = line.split(',');
+
+        string element = "";
+        switch (parsed[1]) {
+            case "f": { element = "fire"; } break;
+            case "w": { element = "water"; } break;
+            case "t": { element = "thunder"; } break;
+            default: {} break;
+        }
+        news ~= JSONValue( [
+            "name": parsed[0].idup,
+            "element": element,
+            "rank": parsed[2].idup
+        ] );
+    }
+
+    foreach( page; json["pages"].array ) {
+        foreach( card; page["cards"].array) {
+            news ~= card;
+        }
+    }
+
+    auto chunked = news.chunks(10);
+
+    JSONValue[] generated = [];
+    foreach( i, chunk; chunked.array ) {
+        JSONValue[] cards = [];
+        foreach( j, card; chunk ) {
+            card.object["row"] = j;
+            cards ~= card;
+        }
+
+        auto gen = JSONValue([ "page": i ]);
+        gen.object["cards"] = cards;
+        generated ~= gen;
+    }
+
+    writeln( generated );
 }
