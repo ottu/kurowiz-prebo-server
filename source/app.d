621f5b19608ccdd49a038339c42559247076c246
ottu <ottottu@gmail.com>
2015-03-24 17:54:30 +0900

    add view files.

--- source/app.d
+++ source/app.d
@@ -4,15 +4,15 @@ import prebo;
 
 shared static this()
 {
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+    auto settings = new HTTPServerSettings;
+    settings.port = 8080;
+    settings.bindAddresses = ["::1", "127.0.0.1"];
+    listenHTTP(settings, &hello);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+    logInfo("Please open http://127.0.0.1:8080/ in your browser.");
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.writeBody("Hello, World!");
+    res.render!("index.dt", req);
 }
