85a1f74a8cbf8f759663cefac360321cf9c9fd36
ottu <ottottu@gmail.com>
2015-03-28 02:13:01 +0900

    working collapse!

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@
     $('#test-hook').on('click', function() {
         $('#test').toggleClass("open");
     });
+    $("#menu-button").click(function(){
+        $("#bs-navbar-collapse").toggleClass("in");
+    });
 })(jQuery)
 
 
