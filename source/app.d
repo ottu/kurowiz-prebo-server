7cf9bdf8949c80b6cc80776f213af7a1090d9d13
ottu <ottottu@gmail.com>
2015-03-27 20:46:45 +0900

    add jquery.min.js, and routing for publics.

--- source/app.d
+++ source/app.d
@@ -1,15 +1,22 @@
 import vibe.d;
+import vibe.core.log;
 
 import prebo;
 
 shared static this()
 {
+    auto router = new URLRouter;
+    router.get("/", &hello);
+    router.get("*", serveStaticFiles("public/"));
+
     auto settings = new HTTPServerSettings;
     settings.port = 8080;
     settings.bindAddresses = ["::1", "127.0.0.1"];
-    listenHTTP(settings, &hello);
 
-    logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+    auto logger = cast(shared)( new FileLogger("access.log") );
+    registerLogger(logger);
+
+    listenHTTP( settings, router );
 }
 
 void hello(HTTPServerRequest req, HTTPServerResponse res)
