37acf37bec201526c89e7f124d42c3abe1cf0680
ottu <ottottu@gmail.com>
2015-03-28 22:01:30 +0900

    add filter of name.

--- public/js/main.js
+++ public/js/main.js
@@ -16,6 +16,30 @@
         $(this).toggleClass("btn-default");
         $(this).toggleClass(cls);
     });
+    $("#name-input-groups").on("click", 'button', function(){
+        var role_span = $(this).find("span");
+        console.log( role_span.hasClass("glyphicon-plus") );
+        if (role_span.hasClass("glyphicon-plus")) {
+            $("#name-input-groups").append( [
+                '<tr btn-role="add">',
+                    '<td>',
+                        '<div class="input-group" id="name-input-group">',
+                            '<span class="input-group-btn">',
+                                '<button class="btn btn-default" type="button">',
+                                    '<span class="glyphicon glyphicon-plus" />',
+                                '</button>',
+                            '</span>',
+                            '<input class="form-control" type="text" />',
+                        '</div>',
+                    '</td>',
+                '</tr>'
+            ].join(''));
+            role_span.removeClass("glyphicon-plus");
+            role_span.addClass("glyphicon-minus");
+        } else {
+            $(this).parent().parent().parent().parent().remove();
+        }
+    });
 })(jQuery)
 
 
