581979ee538657c1451c55c4857ce5290906cd73
ottu <ottottu@gmail.com>
2015-03-28 18:37:15 +0900

    implemented toogle element buttons.

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,20 @@
 ;(function(){
     $('#test-hook').on('click', function() {
+        console.log("test");
         $('#test').toggleClass("open");
     });
     $("#menu-button").click(function(){
         $("#bs-navbar-collapse").toggleClass("in");
+    });
+    $("label[id^=element-button]").on('click', function(){
+        var cls = "";
+        switch ($(this).text().trim()) {
+            case "火": { cls = "btn-danger" } break;
+            case "水": { cls = "btn-info" } break;
+            case "雷": { cls = "btn-warning" } break;
+        }
+        $(this).toggleClass("btn-default");
+        $(this).toggleClass(cls);
     });
 })(jQuery)
 
