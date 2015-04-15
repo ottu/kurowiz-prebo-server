ee4b5afc38a9c5ae29285522493a3abc6e1004a4
ottu <ottottu@gmail.com>
2015-04-15 22:47:25 +0900

    wip

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,4 @@
 ;(function(){
-    $('#test-hook').on('click', function() {
-        console.log("test");
-        $('#test').toggleClass("open");
-    });
-    $("#menu-button").click(function(){
-        $("#bs-navbar-collapse").toggleClass("in");
-    });
-    $("label[id^=element-button] input").on('click', function(){
-        var label = $(this).parent();
-        var id = label.attr("id");
-        var cls = "";
-        switch (id) {
-            case "element-button-fire": { cls = "btn-danger" } break;
-            case "element-button-water": { cls = "btn-info" } break;
-            case "element-button-thunder": { cls = "btn-warning" } break;
-            case "element-button-none": { cls = "btn-success" } break;
-        }
-        label.toggleClass("btn-default");
-        label.toggleClass(cls);
-    });
-    $("label[id$=-button] input").on('click', function(){
-        var label = $(this).parent();
-        label.toggleClass("btn-default");
-        label.toggleClass("btn-primary");
-    });
     $("#name-input-groups").on("click", 'button', function(){
         var role_span = $(this).find("span");
         console.log( role_span.hasClass("glyphicon-plus") );
@@ -45,5 +20,3 @@
         }
     });
 })(jQuery)
-
-
